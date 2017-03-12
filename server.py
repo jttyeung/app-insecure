3ae3a915fd1de5d0cd77dd401101ffc6b7436dd3
Joanne Yeung <jttyeung@gmail.com>
2017-03-12 14:42:38 -0700

    added base template and server

--- server.py
+++ server.py
@@ -1,23 +1,33 @@
-""" Web Insecure Server """
+""" App Insecure Server """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
-
 import jinja2
 
 
 app = Flask(__name__)
 
+# jinja debugger
+app.jinja_env.undefined = jinja2.StrictUndefined
+app.jinja_env.auto_reload = True
+
+@app.route('/')
+def homepage():
+    """ Brings user to the homepage. """
+    return render_template("index.html")
 
 
+@app.route('/levelzero')
+def levelzero():
+    """ Brings user to the homepage. """
+    return render_template("levelzero.html")
 
 
 if __name__ == "__main__":
     app.debug = True
 
-    connect_to_db(app)
-
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=4000)
